2c1c7bb0e1d5c7b406d1967c30e577d550455fc8
Ghazaleh-Graziella <ghazale.f@yahoo.com>
2022-11-12 20:53:48 +0330

    cerate login and register form

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,33 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
+    meta: {
+      requireAuth: true,
+    },
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    path: "/about",
+    name: "about",
+    component: () => import("../views/AboutView.vue"),
+    meta: {
+      requireAuth: true,
+    },
+  },
+  {
+    path: "/login",
+    name: "login",
+    component:  import("../views/LoginView.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,12 @@
 /* eslint-disable no-unused-vars */
 import { createStore } from "vuex";
-
+import router from "../router";
+import { auth } from "../firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "@firebase/auth";
 export default createStore({
   state: {
     user: null,
@@ -16,13 +22,54 @@ export default createStore({
   },
   actions: {
     async login({ commit }, detailes) {
-      //
+      const { email, password } = detailes;
+      try {
+        await signInWithEmailAndPassword(auth, email, password);
+      } catch (error) {
+        switch (error.code) {
+          case "auth/user-not-found":
+            alert("user not found");
+            break;
+          case "auth/wrong-password":
+            alert("somthing went wrong");
+        }
+        return;
+      }
+      commit("SET_USER", auth.currentUser);
+
+      router.push("/");
     },
-    async register({ commit }, details) {
-      //
+    async register({ commit }, detailes) {
+      const { email, password } = detailes;
+      try {
+        await createUserWithEmailAndPassword(auth, email, password);
+      } catch (error) {
+        switch (error.code) {
+          case "auth/email-already-in-use":
+            alert("Email already in use");
+            break;
+          case "auth/invalid-email":
+            alert("Invalid email");
+            break;
+          case "auth/operation-not-allowed":
+            alert("Operation not allowed");
+            break;
+          case "auth/weak-password":
+            alert("Weak password");
+            break;
+          default:
+            alert("Something went wrong");
+        }
+        return;
+      }
+      commit("SET_USER", auth.currentUser);
+
+      router.push("/");
     },
     async logout({ commit }) {
-      //
+      await signOut(auth);
+      commit("CLAEAR_USER");
+      router.push("/login");
     },
   },
 });
